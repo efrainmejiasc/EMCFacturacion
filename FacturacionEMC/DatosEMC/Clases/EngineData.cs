1977f766cd62285cef922dad2726438ece27a55b
_efrainmejiasc <85888811+EfrainAS@users.noreply.github.com>
2021-09-27 10:39:06 -0400

    INICIO

---
+++ FacturacionEMC/DatosEMC/Clases/EngineData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosEMC.Clases
+{
+    public class EngineData
+    {
+        private static EngineData valor;
+        public static EngineData Instance()
+        {
+            if ((valor == null))
+            {
+                valor = new EngineData();
+            }
+            return valor;
+        }
+    }
+}
---
+++ FacturacionEMC/DatosEMC/DataModels/AppContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosEMC.DataModels
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options)
+           : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(EngineData.ConnectionDb);
+            }
+
+        }
+    }
+}
