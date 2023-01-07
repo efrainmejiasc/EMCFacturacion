e32692f039db5e4292ecd4475301ad494c24f3ca
efrain.mejias@dirmod.com <efrain.mejias@dirmod.com>
2023-01-06 22:16:18 -0400

    update

--- FacturacionEMC/FacturacionEMCSite/wwwroot/js/scriptsSite/productosManager/jsEditProductosManager.js
+++ FacturacionEMC/FacturacionEMCSite/wwwroot/js/scriptsSite/productosManager/jsEditProductosManager.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function () {
     console.log("ready!");
+    $('#modalEdicion').hide();
     setTimeout(DocumentoListo, 2000);
 });
 
@@ -27,9 +28,9 @@ function GetInfoImagenes() {
                       <td> ${item.id} </td>
                       <td> ${item.descripcion} </td>
                       <td> ${item.categoria} </td>
-                      <td> <img  id='${item.id}' src='${item.strBase64}'  class="img-fluid img-thumbnail" style="width:70%; min-width:70px; min-height:50px; height:auto;"/></td>
-                      <td>  <input type="button" onclick="Editar();" class="btn btn-warning" value="Editar" /> </td>
-                      <td>  <input type="button" onclick="Eliminar();" class="btn btn-danger" value="Eliminar"  /> </td>
+                      <td> <img  id='${item.id}' src='${item.strBase64}'  class="img-fluid img-thumbnail" style="width:40%; min-width:70px; min-height:25px; height:auto;"/></td>
+                      <td>  <input type="button" onclick="Editar();" class="btn btn-warning" value="Editar"   style="width:125px;" /> </td>
+                      <td>  <input type="button" onclick="Eliminar();" class="btn btn-danger" value="Eliminar" style="width:125px;" /> </td>
                       </tr>`;
                     $('#tablaImg tbody').append(tr);
                 });
@@ -46,4 +47,16 @@ function GetInfoImagenes() {
     $("#tablaImg").addClass("display compact dt-center");
 
     return false;
+}
+
+function Editar() {
+    $('#modalEdicion').show();
+}
+
+function CloseEditar() {
+    $('#modalEdicion').hide();
+}
+
+function Eliminar() {
+
 }
