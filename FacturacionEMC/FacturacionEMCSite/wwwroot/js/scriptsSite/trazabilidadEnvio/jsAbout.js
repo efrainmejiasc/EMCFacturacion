57efe56f302943f9549d0e25eb850b0fe55bf858
efrain.mejias@dirmod.com <efrain.mejias@dirmod.com>
2023-04-26 10:48:59 -0400

    Update_26042023

--- FacturacionEMC/FacturacionEMCSite/wwwroot/js/scriptsSite/trazabilidadEnvio/jsAbout.js
+++ FacturacionEMC/FacturacionEMCSite/wwwroot/js/scriptsSite/trazabilidadEnvio/jsAbout.js
@@ -39,6 +39,10 @@ function ObtenerTrazabilidadesEnvios() {
         return false;
     }
 
+    var activo = '';
+    var eliminar = cultureInfo == 'en-US' ? 'Remove' : 'Eliminar';
+    var editar = cultureInfo == 'en-US' ? 'Edit' : 'Editar';
+
     $.ajax({
         type: "GET",
         url: urlObtenerTrazabilidadesEnvios,
@@ -46,11 +50,64 @@ function ObtenerTrazabilidadesEnvios() {
         datatype: "json",
         success: function (data) {
             if (data != null) {
-                console.log(data); 
+                $('#tablaRegistros tbody tr').remove();
+                $.each(data, function (index, item) {
+                    activo = item.activo ? 'green' : 'red';
+                    let tr = `<tr>
+                      <td style='color: ${activo}'> ${item.id} </td>
+                      <td style='color: ${activo}'> ${item.nombre} </td>
+                      <td style='color: ${activo}'> ${item.dni} </td>
+                      <td style='color: ${activo}'> ${item.direccion} </td>
+                      <td style='color: ${activo}'> ${item.telefono} </td>
+                      <td style='color: ${activo}'> ${item.email} </td>
+                      <td style='color: ${activo}'> ${item.fechaEnvio.substring(0, 10)} </td>
+                      <td style='color: ${activo}'> ${item.fechaLlegada.substring(0, 10)} </td>
+                      <td style='color: ${activo}'> ${item.observacion} </td>
+                     <td><a href="javascript:void(0)" class="btn btn-sm btn-warning" onclick="Editar(${item.id})">${editar}</a></td>
+                     <td><a href="javascript:void(0)" class="btn btn-sm btn-danger" onclick="Quitar(${item.id})">${eliminar}</a></td>
+                      </tr>`;
+                    $('#tablaRegistros tbody').append(tr);
+                });
+                setTimeout(InicializarDataTable, 1000);
             }
+            else {
+                $('#tablaRegistros tbody tr').remove();
+            }
+        }, error: function (jqXHR, textStatus, errorThrown) {
+            $('#tablaRegistros tbody tr').remove();
         }
     });
 
+    $("#tablaRegistros").addClass("display compact dt-center");
     return false;
+}
+
+function InicializarDataTable() {
+    var init = $('#initDataTable').val();
+
+    try {
+        if (init === 'no') {
+            $('#tablaRegistros').DataTable({
+                language: {
+                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
+                },
+                "bInfo": false,
+                "lengthChange": false,
+                pagingType: "simple"
+            });
+            $('#initDataTable').val('si');
+        } else {
+            $('#tablaRegistros').DataTable().fnDestroy({
+                language: {
+                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
+                },
+                "bInfo": false,
+                "lengthChange": false,
+                pagingType: "simple"
+            });
+        }
+    } catch { console.log(''); }
+
+    $("#tablaRegistros").addClass("display compact dt-center");
 }
 
