ee2ab153421a86e02e11f6ecbe9cd357007820db
efrain.mejias@dirmod.com <efrain.mejias@dirmod.com>
2023-06-20 14:17:14 -0400

    update

---
+++ FacturacionEMC/DatosEMC/Migrations/20230620181525_loteria4.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatosEMC.Migrations
+{
+    public partial class loteria4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ticket",
+                table: "VentaNumero",
+                type: "VARCHAR(50)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ticket",
+                table: "VentaNumero");
+        }
+    }
+}
